70ea242cccd72d3a2b1532262e35268fe48867a3
Asus <Asus@EZCAPE>
2023-09-20 17:20:58 +0800

    Add project files.

---
+++ uidesign/AboutMe.cs
@@ -0,0 +1,15 @@
+namespace uidesign
+{
+    public partial class AboutMe : Form
+    {
+        public AboutMe()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
